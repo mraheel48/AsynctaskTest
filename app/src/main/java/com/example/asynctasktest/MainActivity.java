ecbb5ac1d9ff360bec362cc195f742490aeabded
hp <m.raheel48@yahoo.com>
2021-02-23 23:00:10 +0500

    Initial commit

---
+++ app/src/main/java/com/example/asynctasktest/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.asynctasktest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.lang.ref.WeakReference;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progress_bar);
+    }
+
+    public void startAsyncTask(View v) {
+        AsyncTaskTest task = new AsyncTaskTest(this);
+        task.execute(10);
+
+    }
+
+
+    private static class AsyncTaskTest extends AsyncTask<Integer, Integer, String> {
+
+        private WeakReference<MainActivity> activityWeakReference;
+
+        AsyncTaskTest(MainActivity activity) {
+            activityWeakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            MainActivity activity = activityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
+            }
+            Log.e("myTag", Thread.currentThread().toString());
+            activity.progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(Integer... integers) {
+            Log.e("myTag", Thread.currentThread().toString());
+            for (int i = 0; i < integers[0]; i++) {
+                publishProgress((i * 100) / integers[0]);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return "Finished!";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            Log.e("myTag", Thread.currentThread().toString());
+            MainActivity activity = activityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
+            }
+            activity.progressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.e("myTag", Thread.currentThread().toString());
+            MainActivity activity = activityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
+            }
+            Toast.makeText(activity, s, Toast.LENGTH_SHORT).show();
+            activity.progressBar.setProgress(0);
+            activity.progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+}
